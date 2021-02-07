293c4a7414fa42bd3733adbc11f08b01a761e48e
dariodasilva <dariotecinfo@gmail.com>
2021-02-07 17:48:30 -0300

    Criação do componente Restaurantes, rota restaurante, routerlink restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import {Routes} from '@angular/router'
 import { AboutComponent } from './about/about.component'
 import { HeaderComponent } from './header/header.component'
 import { HomeComponent } from './home/home.component'
+import { RestaurantesComponent } from './restaurantes/restaurantes.component'
 
 export const ROUTES: Routes = [
 
     {path: '', component: HomeComponent},
     {path: '', component: HeaderComponent},
     {path: 'about', component: AboutComponent},
+    {path: 'restaurantes', component: RestaurantesComponent}
 
 ]
