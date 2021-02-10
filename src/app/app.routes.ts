6f040f4f6d7b015d21ade99fc94214ba67be6552
dariodasilva <dariotecinfo@gmail.com>
2021-02-10 16:43:45 -0300

    Componentes Menu detail, Shopping Cart e Menu item detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component'
 import { HomeComponent } from './home/home.component'
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 
 export const ROUTES: Routes = [
 
@@ -11,6 +12,7 @@ export const ROUTES: Routes = [
     {path: '', component: HeaderComponent},
     {path: 'about', component: AboutComponent},
     {path: 'restaurantes', component: RestaurantsComponent},
-    {path: 'restaurant', component: RestaurantComponent}
+    {path: 'restaurant', component: RestaurantComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent},
 
 ]
