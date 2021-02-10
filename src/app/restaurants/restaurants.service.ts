55314f29a7e7050330038f83fad52ceb4715e068
dariodasilva <dariotecinfo@gmail.com>
2021-02-09 21:51:11 -0300

    Adição do Observable, Http, rxjs, map e subscribe

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,34 +1,24 @@
+import {Injectable} from '@angular/core';
+
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+
+import {Http} from '@angular/http';
+
 import { Restaurant } from "./restaurant/restaurant.model";
 
+import {MEAT_API} from 'app/app.api';
+
+
+@Injectable()
 export class RestaurantsService {
 
-    rests: Restaurant[] = [    
-        {
-        id: "bread-bakery",
-        name: "Bread & Bakery",
-        category: "Bakery",
-        deliveryEstimate: "25m",
-        rating: 4.9,
-        imagePath: "assets/img/restaurants/breadbakery.png",
-        about: "A Bread & Brakery tem 40 anos de mercado. Fazemos os melhores doces e pães. Compre e confira.",
-        hours: "Funciona de segunda à sexta, de 8h às 23h"
-      },
-      {
-        id: "burger-house",
-        name: "Burger House",
-        category: "Hamburgers",
-        deliveryEstimate: "100m",
-        rating: 3.5,
-        imagePath: "assets/img/restaurants/burgerhouse.png",
-        about: "40 anos se especializando em trash food.",
-        hours: "Funciona todos os dias, de 10h às 22h"
-      },
-    ]
     
-   constructor(){}
+    
+   constructor(private http: Http){}
 
-   restaurants(): Restaurant[] {
-    return this.rests;
-  }
+   restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())  }
   
 }
